020b15c948794fe5e0e2e22600b69cbfe75959a0
B-Houssam <vigenzo47@gmail.com>
2021-07-08 23:37:54 +0100

    datasets import delete and info state api

--- src/pages/dashdata.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashData from "../components/DashBoardData";
-
-const DashD = () => {
-  return <DashData></DashData>;
-};
-
-export default DashD;
--- src/pages/dashmain.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Dash from "../components/DashboardMain";
-
-const DashMain = () => {
-  return <Dash></Dash>;
-};
-
-export default DashMain;
